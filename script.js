54e9a2f2b1e42161d91f7b23c3272c8e8b42dadf
kolozsvari <kolozsvarikrisztinajanka@gmail.com>
2023-05-30 09:04:51 +0200

    cica

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function kiir() {
-    fetch("https://catfact.ninja/fact")
+    fetch("https://catfact.ninja/facts")
     .then(adat => adat.json())
     .then(res => {
         for (const item of res ["data"]){
